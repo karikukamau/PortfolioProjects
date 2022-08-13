923432dcc1616e8126248cadfd27aa3f553e4481
karikukamau <111172273+karikukamau@users.noreply.github.com>
2022-08-13 08:34:32 +0100

    Delete Global COVID Data.sql

--- Global COVID Data.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[vaccination_trend]
-      ,[vaccination_rate]
-  FROM [PortfolioProject].[dbo].[PercentVaccinations]
